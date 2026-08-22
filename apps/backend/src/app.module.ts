7a9e1ec36730db2eec1c27ae0b4f143041ef8ee5
shpvk <138403371+shpvk@users.noreply.github.com>
2026-08-23 00:43:54 +0300

    refactor(backend): replace TypeORM with Prisma and a config-driven bootstrap

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,24 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { BuildsModule } from './builds/builds.module';
-import { CommentsModule } from './comments/comments.module';
-import { databaseConfig } from './database/data-source';
 import { HeroesModule } from './heroes/heroes.module';
-import { VotesModule } from './votes/votes.module';
+import {ConfigModule} from "@nestjs/config";
+import {IS_DEV_ENV} from "./libs/common/utils/is-dev.utils";
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            ...databaseConfig,
-            autoLoadEntities: true,
-            // MVP only: schema is derived from entities instead of migrations.
-            synchronize: true,
+        ConfigModule.forRoot({
+            ignoreEnvFile: !IS_DEV_ENV,
+            isGlobal: true,
         }),
         HeroesModule,
-        BuildsModule,
-        VotesModule,
-        CommentsModule,
         AuthModule,
     ],
 })
