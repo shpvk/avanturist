0a5c933011b853bdf39f438bdd0f5428b477d063
shpvk <138403371+shpvk@users.noreply.github.com>
2026-08-23 23:48:49 +0300

    feat(auth): rebuild the auth module on redis-backed sessions

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,18 +6,51 @@ import {ValidationPipe} from "@nestjs/common";
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 
+import IORedis from 'ioredis'
+import session from "express-session";
+import {ms, StringValue} from "./libs/common/utils/ms.util";
+import {parseBoolean} from "./libs/common/utils/parse-boolean.utils";
+import {RedisStore} from "connect-redis";
+
+
+
 async function bootstrap(): Promise<void> {
 
 
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
+  const redis = new IORedis(config.getOrThrow<string>('REDIS_URI'));
 
   app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+
+  app.use(
+      session({
+        secret: config.getOrThrow<string>('SESSION_SECRET'),
+        name: config.getOrThrow<string>('SESSION_NAME'),
+        resave: true,
+        saveUninitialized: false,
+        cookie: {
+          domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+          maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+          httpOnly: parseBoolean(
+              config.getOrThrow<string>('SESSION_HTTP_ONLY')
+          ),
+          secure: parseBoolean(
+              config.getOrThrow<string>('SESSION_SECURE')
+          ),
+          sameSite: 'lax'
+        },
+          store: new RedisStore({
+              client: redis,
+              prefix: config.getOrThrow('SESSION_FOLDER')
+          }),
+      })
+  )
 
   app.enableCors({
     origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
