d3763740896d8c677716837ece6b4147874574fb
shpvk <138403371+shpvk@users.noreply.github.com>
2026-08-23 23:49:02 +0300

    feat(backend): register prisma and user modules in the app

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { HeroesModule } from './heroes/heroes.module';
 import {ConfigModule} from "@nestjs/config";
 import {IS_DEV_ENV} from "./libs/common/utils/is-dev.utils";
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             ignoreEnvFile: !IS_DEV_ENV,
             isGlobal: true,
+            expandVariables: true,
         }),
         HeroesModule,
         AuthModule,
+        PrismaModule,
+        UserModule,
     ],
 })
 export class AppModule {}
