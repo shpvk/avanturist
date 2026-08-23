1751dd09ab4695ea31ebc3994564aaba774bcdce
shpvk <138403371+shpvk@users.noreply.github.com>
2026-08-23 23:48:26 +0300

    feat(db): add prisma module backed by the postgres adapter

---
+++ apps/backend/src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import {Injectable, OnModuleDestroy, OnModuleInit} from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client'
+import {ConfigService} from "@nestjs/config";
+import {PrismaPg} from "@prisma/adapter-pg";
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy {
+
+    constructor(config: ConfigService) {
+        const adapter = new PrismaPg({
+            connectionString: config.getOrThrow<string>('POSTGRES_URI'),
+        });
+
+        super({ adapter });
+    }
+
+    public async onModuleInit(): Promise<void> {
+        await this.$connect()
+    }
+
+    public async onModuleDestroy(): Promise<void> {
+        await this.$disconnect()
+    }
+}
